09175454d6a3745a5fc4f769456403eeaca00efe
Arabinda MOHAPATRA <arabindamohapatra@outlook.com>
2025-01-10 11:29:59 +0000

    snapshot has been commited

---
+++ models/example/macros_orders.sql
@@ -0,0 +1,24 @@
+
+
+{{ config(
+    materialized='table'
+) }}
+
+SELECT
+    o.Order_ID,
+    c.Customer_ID,
+    c.Customer_Name,
+    p.Product_ID,
+    p.Product_Name,
+    o.Quantity,
+    o.Order_Date,
+     {{get_date_parts('Order_Date')}} as date_extract
+
+FROM
+    `regal-tract-377101.dbt_amohapatra.orders` o
+JOIN
+    `regal-tract-377101.dbt_amohapatra.stg_customers` c 
+    ON o.Customer_ID = c.Customer_ID
+JOIN
+    `regal-tract-377101.dbt_amohapatra.products` p  -- Fully qualified dataset reference
+    ON o.Product_ID = p.Product_ID
